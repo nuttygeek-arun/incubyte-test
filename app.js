9d6c31dcd65d78d97e06d7ef6814739362878157
Arun Sharma <nuttygeek.arun@gmail.com>
2025-03-06 22:05:15 +0530

    step 1 with , saperated value with test cases

---
+++ app.js
@@ -0,0 +1,12 @@
+ function add(numString) {
+    if(numString === '') return 0;
+    const numArray = numString.split(',');
+    return numArray.map((num) => parseInt(num)).reduce((acc, val) => {
+        return acc+val
+    }, 0);
+}
+
+const sum = add('5,6');
+console.log(":: sum", sum)
+
+module.exports = add;
